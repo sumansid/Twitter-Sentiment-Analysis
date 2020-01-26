86345921b1f04ee614e992af8999d12a98b47895
sumansid <53033648+sumansid@users.noreply.github.com>
2020-01-26 07:37:23 -0500

    Add files via upload

---
+++ Twitter Sentiment Analyser/Twitter Sentiment Analyser/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Twitter Sentiment Analyser
+//
+//  Created by Suman Sigdel on 1/26/20.
+//  Copyright © 2020 Suman Sigdel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var sentimentLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func checkButtonPressed(_ sender: UIButton) {
+    }
+    
+
+
+}
+
---
+++ TwitterSentimentModel.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+import CreateML
+
+let data = try MLDataTable(contentsOf:URL(fileURLWithPath:"/Users/sumansigdel/Downloads/twitter-sanders-apple3.csv"))
+
+let(trainingData, testingData) = data.randomSplit(by: 0.7, seed: 5)
+let sentimentClassifier = try MLTextClassifier(trainingData: trainingData, textColumn: "text", labelColumn: "class")
+let evaluationMetrics = sentimentClassifier.evaluation(on: testingData, textColumn: "text", labelColumn: "class")
+let accuracy = (1.0 - evaluationMetrics.classificationError) * 100
+let metaData = MLModelMetadata(author: "Suman Sigdel", shortDescription: "Twitter Sentiment Analyser Model", version: "1.0")
+try sentimentClassifier.write(to: URL(fileURLWithPath: "/Users/sumansigdel/Downloads/TwitterSentimentClassifier.mlmodel"))
+
+try sentimentClassifier.prediction(from: "RSR coin will dump soon")
+
+try sentimentClassifier.prediction(from: "I love cocacola")
