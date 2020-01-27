a3a1770ca638578a5ec706fec899dd94159f18db
sumansid <53033648+sumansid@users.noreply.github.com>
2020-01-27 12:08:48 -0500

    Final App with all the features integrated
    The final app with emotions and the sentiment score

---
+++ Twitter Sentiment Analyser/Pods/Target Support Files/Pods-Twitter Sentiment Analyser/Pods-Twitter Sentiment Analyser-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Twitter_Sentiment_Analyser : NSObject
+@end
+@implementation PodsDummy_Pods_Twitter_Sentiment_Analyser
+@end
--- Twitter Sentiment Analyser/Twitter Sentiment Analyser/ViewController.swift
+++ Twitter Sentiment Analyser/Twitter Sentiment Analyser/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import SwifteriOS
+import CoreML
+
 
 class ViewController: UIViewController {
     
@@ -15,12 +18,78 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     
+    let tSentimentClassifier = TwitterSentimentClassifier()
+    // Instantiation using Twitter's OAuth Consumer Key and secret
+    let swifter = Swifter(consumerKey: "LTReJyreyyuz4SWjVABuA1WKI", consumerSecret: "ajf107xfd6869heCG8Ptn8iztfFHBWPIxLkFGvsmoEwI7MKZcz")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        let prediction = try! tSentimentClassifier.prediction(text: "I hate @apple")
+//        print(prediction.label)
+        
         // Do any additional setup after loading the view.
+       
     }
     
     @IBAction func checkButtonPressed(_ sender: UIButton) {
+        if let searchText = textField.text {
+                   swifter.searchTweet(using: searchText, lang:"en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+                       
+                       var tweets = [TwitterSentimentClassifierInput]()
+                       
+                       for i in 0..<100 {
+                           if let tweet = results[i]["full_text"].string {
+                               let tweetForModel = TwitterSentimentClassifierInput(text: tweet)
+                               tweets.append(tweetForModel)
+                               }
+                           
+                       }
+                       do {
+                        
+                           let predictions = try self.tSentimentClassifier.predictions(inputs: tweets)
+                           var sentimentScore = 0
+                           for prediction in predictions {
+                               let sentiment = prediction.label
+                               if sentiment == "Pos" {
+                                   sentimentScore += 1
+                               } else if sentiment == "Neg" {
+                                   sentimentScore -= 1
+                               }
+                               
+                           }
+                        print(sentimentScore)
+                        if sentimentScore > 15 {
+                            self.sentimentLabel.text = "😍"
+                        } else if sentimentScore > 5 {
+                            self.sentimentLabel.text = "😊"
+                        } else if sentimentScore > 0 {
+                            self.sentimentLabel.text = "🙂"
+                        } else if sentimentScore > -5 {
+                            self.sentimentLabel.text = "😕"
+                        } else if sentimentScore > -10 {
+                            self.sentimentLabel.text = "😈"
+                        }else if sentimentScore > -15 {
+                            self.sentimentLabel.text = "☹️"
+                        }else if sentimentScore > -25 {
+                            self.sentimentLabel.text = "😡"
+                        }else {
+                            self.sentimentLabel.text = "😡💩🤮"
+                            
+                        }
+                           
+                       } catch {
+                           print("An error making the prediction\(error)")
+                           
+                       }
+                        
+                       
+                   }) { (error) in
+                       print("Error with the twitter api request \(error)")
+                   }
+                   
+               }
+               
     }
     
 
